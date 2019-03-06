86550d9229b690f92d36258992dbfea93deb684c
Rushil Perera <pererarushil1940@gmail.com>
2019-03-06 10:43:17 -0500

    Modified latestEvents to return previous events if not enough recent events

--- resolvers/queries/event.js
+++ resolvers/queries/event.js
@@ -1,16 +1,28 @@
 const { Datastore } = require('@google-cloud/datastore');
+const dayjs = require('dayjs');
+
 const { GCP_PROJECT_ID } = process.env;
 const datastore = new Datastore({ projectId: GCP_PROJECT_ID });
 const kind = 'Events';
 
+const latestEvents = async (minLimit = 4, operator = '>') => {
+    const query = datastore.createQuery(kind)
+        .order('id', { descending: true })
+        .limit(minLimit)
+        .filter('id', operator, dayjs().format('YYYY-MM-DD'));
+
+    let [events] = await datastore.runQuery(query);
+    if (events.length < minLimit && operator === '>') {
+        const moreEvents = await latestEvents(minLimit - events.length, '<');
+        events = events.concat(moreEvents);
+    }
+
+    return events;
+};
+
 const resolvers = {
     Query: {
-        latestEvents: async (minLimit = 4) => {
-            const query = datastore.createQuery(kind).order('id', { descending: true });
-            const [events] = await datastore.runQuery(query);
-
-            return events;
-        }
+        latestEvents: (obj, { minLimit = 4 }) => latestEvents(minLimit),
     }
 }
 
