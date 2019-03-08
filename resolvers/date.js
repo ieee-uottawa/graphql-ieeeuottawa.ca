28f14c308fb408261309f6fdee683f7cd424139c
Rushil Perera <pererarushil1940@gmail.com>
2019-03-08 11:57:45 -0500

    Modified Date to use dayjs

--- resolvers/date.js
+++ resolvers/date.js
@@ -1,15 +1,16 @@
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
+const dayjs = require('dayjs');
 
 const resolver = {
     Date: new GraphQLScalarType({
         name: 'Date',
         description: 'Date custom scalar type',
         parseValue(value) {
-            return new Date(value); // value from the client
+            return dayjs(value); // value from the client
         },
         serialize(value) {
-            return value.getTime(); // value sent to the client
+            return value.format('YYYY-MM-DD'); // value sent to the client
         },
         parseLiteral(ast) {
             if (ast.kind === Kind.INT) {
