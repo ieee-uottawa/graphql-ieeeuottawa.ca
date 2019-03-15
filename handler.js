ed57624179ba96f5de155ddad50e5e6430eb5665
Rushil Perera <pererarushil1940@gmail.com>
2019-03-15 16:28:01 -0400

    Modified backend to handle nomination submissions

--- handler.js
+++ handler.js
@@ -1,13 +1,51 @@
-require('dotenv').load();
-const express = require('express');
+const request = require('superagent');
 
-const server = require('./schema');
-const { GRAPHQL_PORT } = require('./config');
+module.exports.submitNomination = async (event) => {
+    const { name, email, studentNumber, program, year, platform, position } = JSON.parse(event.body);
 
-const app = express();
+    const { origin } = event.headers;
+    const allowedOrigins = ['http://localhost:8000', 'https://ieeeuottawa.ca'];
+    const headers = {
+        'Access-Control-Allow-Credentials': true,
+    };
+    if (allowedOrigins.indexOf(origin) > -1) {
+        headers['Access-Control-Allow-Origin'] = origin;
+    } else {
+        return {
+            statusCode: 403,
+            headers,
+            isBase64Encoded: false,
+            body: JSON.stringify({ success: false }),
+        };
+    }
+    console.log(event.headers, origin, allowedOrigins.indexOf(origin) > -1, headers);
 
-server.applyMiddleware({ app });
-
-app.listen(GRAPHQL_PORT, () => {
-    console.log(`GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphql`);
-});
+    try {
+        const res = await request.post('https://docs.google.com/forms/d/e/1FAIpQLSe7yw4EC08qpF3BEJVUMhKQDmu6ubIJidkD8jPrdeL8Y2h8iA/formResponse')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({
+                'entry.1463790818': name,
+                'entry.304218284': email,
+                'entry.143175545': studentNumber,
+                'entry.1924293788': program,
+                'entry.787289669': year,
+                'entry.1315432807': position,
+                'entry.463459026': platform,
+            });
+        console.log(res.status);
+        return {
+            statusCode: res.status,
+            headers,
+            body: JSON.stringify({ success: true }),
+            isBase64Encoded: false,
+        };
+    } catch (error) {
+        console.log(error.status);
+        return {
+            statusCode: 500,
+            headers,
+            body: JSON.stringify({ success: false }),
+            isBase64Encoded: false,
+        };
+    }
+};
